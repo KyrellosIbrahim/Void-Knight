1c90848d3634816a2a0c5a9f884feaf279c73490
Kyrellos Saad Ibrahim <kyrellosibrahim@gmail.com>
2026-04-27 19:25:52 -0500

    actual music script

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+// for the sake of keeping music playing across scene loads so it doesn't restart every new scene.
+public class MusicPlayer : MonoBehaviour
+{
+    private static MusicPlayer instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
